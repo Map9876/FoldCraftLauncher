98d30c41924a125bfa1d67db9d3e8a1bbb7db3d0
Map9876 <hhdheuwjbgg@outlook.com>
2025-02-10 04:32:54 +0800

    Update MultiMCModpackInstallTask.java

--- FCLCore/src/main/java/com/tungsten/fclcore/mod/multimc/MultiMCModpackInstallTask.java
+++ FCLCore/src/main/java/com/tungsten/fclcore/mod/multimc/MultiMCModpackInstallTask.java
@@ -1,20 +1,3 @@
-/*
- * Hello Minecraft! Launcher
- * Copyright (C) 2020  huangyuhui <huanghongxun2008@126.com> and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package com.tungsten.fclcore.mod.multimc;
 
 import com.google.gson.JsonParseException;
@@ -242,5 +225,20 @@ public final class MultiMCModpackInstallTask extends Task<Void> {
             }
         }
         throw new IllegalStateException("packwiz-installer-bootstrap.jar path not found in instance.cfg");
+    }
+
+    /**
+     * 更新 modpack.cfg 文件中的 jvmArgs 参数
+     */
+    private void updateModpackCfg(String gameDirectory, String javaAgentArgument) throws IOException {
+        Path modpackCfgPath = Paths.get(gameDirectory, "modpack.cfg");
+        List<String> lines = Files.readAllLines(modpackCfgPath);
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).contains("\"jvmArgs\"")) {
+                lines.set(i, "    \"jvmArgs\": \"" + javaAgentArgument + "\",");
+                break;
+            }
+        }
+        Files.write(modpackCfgPath, lines);
     }
         }
